afc7446f18014441176848e712a4493c3ceab5ef
Raj Singh <82027589+Raj-Singh08@users.noreply.github.com>
2024-12-28 13:12:49 +0000

    02_dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict";   //treat all JS code as newer version
+
+//alert(3+3)    //we are using nodeJs not browser
+
+// number 
+// bigint
+// string=> ""
+// boolean=> true/false
+// null => standalone value(Represntation of empty value)(intentionally)
+// Undefined => value is not assigned
+// Symbol =>unique (used in react and figma)
+
+//object
+
+
+console.log(typeof null)    //object
+console.log(typeof undefined)   //undefined
