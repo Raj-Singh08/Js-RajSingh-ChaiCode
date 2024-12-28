d2d46cd4c51333b08987e1e375644ba81259eae5
Raj Singh <82027589+Raj-Singh08@users.noreply.github.com>
2024-12-28 13:27:33 +0000

    Conversion of DataType

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score="33abc"
+let valueInNumber=Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1;false=>0
+//null=>0
+
+let isLoggedIn=undefined;
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+//1=>true;0=>false
+//""=>false
+//"Raj"=>true
+//null=>false
+
+
+let someNumber=undefined
+
+let StringNumber=String(someNumber)
+console.log(typeof StringNumber)
+console.log(StringNumber)
