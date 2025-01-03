e87434d6e08ce5774fac84cbdbcc86850cfc8066
Raj Singh <82027589+Raj-Singh08@users.noreply.github.com>
2025-01-03 17:05:09 +0000

    all commit

---
+++ 01_basics/07_Dates.js
@@ -0,0 +1,20 @@
+let date=new Date();
+// console.log(date);
+// console.log(date.toString());
+// console.log(date.toLocaleString());
+// console.log(date.toLocaleDateString());
+// console.log(typeof date);       //Object
+
+let myDate=new Date(2024,0,23);
+let newDate=new Date("2023-01-04")
+// console.log(newDate.toString());
+
+let myTimeStamp=Date.now();
+// console.log(myTimeStamp);
+// console.log(myDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000))
+
+
+
+
---
+++ 03_basics/01_functions.js
@@ -0,0 +1,16 @@
+// function addTwoNumber(number1,number2){
+//     console.log(number1+number2);
+// }
+
+
+
+// function addTwoNumber(number1="5",number2=9){
+//     let result=number1+number2;
+//     return result;
+// }
+// let result=addTwoNumber();
+// console.log(result)
+
+function calculateCartPrice(...num1){
+    return num1;
+}
